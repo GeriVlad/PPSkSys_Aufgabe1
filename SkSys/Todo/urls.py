95a2d768aa84f94b5dbe5e298b02bc2decc76106
James <52463133+JaL11@users.noreply.github.com>
2020-05-21 21:31:41 +0200

    all links working between pages, edit not working... work in progress

--- SkSys/Todo/urls.py
+++ SkSys/Todo/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('overview', views.index, name='index'),
     path('<int:Todo_id>/', views.detail, name='detail'),
+    path('<int:Todo_id>/edit/', views.edit, name='edit'),
+    path('newtodo', views.new, name='new_Todo'),
+    path('impressum', views.impressum, name='impressum'),
 ]
--- SkSys/Todo/views.py
+++ SkSys/Todo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
 from .models import Todo
 
 def index(request):
@@ -9,3 +11,25 @@ def index(request):
 
 def detail(request, Todo_id):
     return HttpResponse("You're looking at Todo %s." % Todo_id)
+
+def edit(request, Todo_id):
+    todo = get_object_or_404(Todo, pk=Todo_id)
+    try:
+        selected_todo = todo.choice_set.get(pk=request.POST)
+    except (KeyError, Todo.DoesNotExist):
+        return render(request, 'todo/detail.html', {
+            'todo': todo,
+            'error_message': "error... something went wrong",
+        })
+    else:  
+        selected_todo.save()
+        return HttpResponseRedirect(reverse('todo:overview'))
+
+
+    return render(request, 'todos/html/edittodo.html', {'todo': todo})
+
+def new(request):
+    return render(request, 'todos/html/newtodo.html')
+
+def impressum(request):
+    return render(request, 'todos/html/impressum.html')
