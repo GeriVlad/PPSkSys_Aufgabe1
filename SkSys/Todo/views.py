27a763dae5aa0b7fcb16adcf7f42f53a5b4bbaed
James <52463133+JaL11@users.noreply.github.com>
2020-05-21 18:10:27 +0200

    overview displays todo's! other pages still to be done

--- SkSys/Todo/views.py
+++ SkSys/Todo/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponse
 from .models import Todo
 
 def index(request):
-    latest_Todo_list = Todo.objects.order_by('-added_on')[:5]
-    output = ', <br/>    '.join([t.todo_text for t in latest_Todo_list])
-    return HttpResponse(output)
+    latest_todo_list = Todo.objects.order_by('-last_modified')[:5]
+    context = {'latest_todo_list': latest_todo_list}
+    return render(request, 'todos/html/overview.html', context)
 
 def detail(request, Todo_id):
     return HttpResponse("You're looking at Todo %s." % Todo_id)
